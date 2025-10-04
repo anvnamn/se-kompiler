ec3b98f22672becbe7dd02835c18d25139b27a6d
anvnamn <85112292+anvnamn@users.noreply.github.com>
2025-10-04 15:10:51 +0200

    Parse function declarations

--- src/parser.cpp
+++ src/parser.cpp
@@ -11,19 +11,53 @@ std::unique_ptr<ExpressionNode> parse_expression(TokenStream &ts) {
   }
 }
 
+std::vector<std::unique_ptr<ParameterNode>> parse_parameters(TokenStream &ts) {
+  std::vector<std::unique_ptr<ParameterNode>> params;
+  while (!dynamic_cast<ClosedParenToken *>(ts.peek())) {
+    Datatype return_type = dynamic_cast<DataTypeToken *>(ts.consume())->type;
+    std::string name = dynamic_cast<IdentifierToken *>(ts.consume())->name;
+    params.emplace_back(std::make_unique<ParameterNode>(
+        return_type, std::make_unique<IdentifierNode>(name)));
+    // If next token is a comma token, consume it.
+    if (dynamic_cast<CommaToken *>(ts.peek())) {
+      ts.consume();
+    }
+  }
+  ts.consume(); // Consume closed paren
+  return params;
+}
+
 Program parse_tokens(TokenStream &ts) {
   std::vector<std::unique_ptr<StatementNode>> statements;
 
   while (ts.peek()) {
     fmt::print("Parsing {}", to_string(*ts.peek()));
 
+    // Return node
     if (dynamic_cast<ReturnToken *>(ts.peek())) {
-      ts.consume();
+      ts.consume(); // Consume return token
       auto return_node = std::make_unique<ReturnNode>(parse_expression(ts));
-      statements.emplace_back(std::move(return_node));
       if (!dynamic_cast<TerminatorToken *>(ts.consume())) {
         throw std::runtime_error("Missing terminator after return statement");
       }
+      statements.emplace_back(std::move(return_node));
+    }
+    // Function declaration
+    else if (dynamic_cast<DataTypeToken *>(ts.peek()) &&
+             dynamic_cast<IdentifierToken *>(ts.peek(1)) &&
+             dynamic_cast<OpenParenToken *>(ts.peek(2))) {
+      Datatype return_type = dynamic_cast<DataTypeToken *>(ts.consume())->type;
+      std::string name = dynamic_cast<IdentifierToken *>(ts.consume())->name;
+      ts.consume(); // consume open paren
+      auto params = parse_parameters(ts);
+      if (!dynamic_cast<TerminatorToken *>(ts.consume())) {
+        throw std::runtime_error(
+            "Missing terminator after function declaration");
+      }
+      auto funcdef_node = std::make_unique<FunctionDeclarationNode>(
+          return_type, std::make_unique<IdentifierNode>(name),
+          std::move(params));
+      statements.emplace_back(std::move(funcdef_node));
     }
   }
   return Program(std::move(statements));
--- tests/parser_test.cpp
+++ tests/parser_test.cpp
@@ -9,9 +9,9 @@ TEST(ReturnStatement, VoidReturn) {
   tokens.emplace_back(std::make_unique<TerminatorToken>());
 
   auto ts = TokenStream{std::move(tokens)};
-  auto root_node = parse_tokens(ts);
+  auto ast = parse_tokens(ts);
 
-  auto return_node = dynamic_cast<ReturnNode *>(root_node.statements[0].get());
+  auto return_node = dynamic_cast<ReturnNode *>(ast.statements[0].get());
   ASSERT_NE(return_node, nullptr) << "Expected a ReturnNode";
 
   ASSERT_EQ(return_node->expression, nullptr)
@@ -21,13 +21,14 @@ TEST(ReturnStatement, VoidReturn) {
 TEST(ReturnStatement, IntReturn) {
   Tokens tokens;
   tokens.emplace_back(std::make_unique<ReturnToken>());
-  tokens.emplace_back(std::make_unique<IntegerLiteralToken>(0));
+  tokens.emplace_back(std::make_unique<IntegerLiteralToken>(1337));
   tokens.emplace_back(std::make_unique<TerminatorToken>());
 
   auto ts = TokenStream{std::move(tokens)};
-  auto root_node = parse_tokens(ts);
+  auto ast = parse_tokens(ts);
 
-  auto return_node = dynamic_cast<ReturnNode *>(root_node.statements[0].get());
+  ASSERT_TRUE(ast.statements.size() == 1);
+  auto return_node = dynamic_cast<ReturnNode *>(ast.statements[0].get());
   ASSERT_NE(return_node, nullptr) << "Expected a ReturnNode";
 
   ASSERT_NE(return_node->expression, nullptr) << "ReturnNode has no expression";
@@ -36,7 +37,7 @@ TEST(ReturnStatement, IntReturn) {
       dynamic_cast<IntegerLiteralNode *>(return_node->expression.get());
   ASSERT_NE(int_expr, nullptr) << "Expected an IntegerLiteralNode";
 
-  EXPECT_EQ(int_expr->value, 0);
+  EXPECT_EQ(int_expr->value, 1337);
 }
 
 TEST(ReturnStatement, MissingTerminator) {
@@ -46,11 +47,53 @@ TEST(ReturnStatement, MissingTerminator) {
 
   auto ts = TokenStream{std::move(tokens)};
   try {
-    auto root_node = parse_tokens(ts);
+    auto ast = parse_tokens(ts);
     FAIL() << "Expected std::runtime_error due to missing return expression";
   } catch (const std::runtime_error &e) {
     EXPECT_THAT(e.what(), testing::ContainsRegex("[Mm]issing terminator"));
   } catch (...) {
     FAIL() << "Expected std::runtime_error, but got a different exception type";
   }
+}
+
+TEST(FunctionDeclaration, VoidReturnNoArgs) {
+  Tokens tokens;
+  tokens.emplace_back(std::make_unique<DataTypeToken>(Datatype::VOID));
+  tokens.emplace_back(std::make_unique<IdentifierToken>("min_funktion"));
+  tokens.emplace_back(std::make_unique<OpenParenToken>());
+  tokens.emplace_back(std::make_unique<ClosedParenToken>());
+  tokens.emplace_back(std::make_unique<TerminatorToken>());
+
+  auto ts = TokenStream{std::move(tokens)};
+  auto ast = parse_tokens(ts);
+
+  ASSERT_TRUE(ast.statements.size() == 1) << fmt::format(
+      "Expected program with 1 statement, actual: {}", ast.statements.size());
+  auto return_node =
+      dynamic_cast<FunctionDeclarationNode *>(ast.statements[0].get());
+  ASSERT_NE(return_node, nullptr) << "Expected a FunctionDeclarationNode";
+  ASSERT_EQ(return_node->returnType, Datatype::VOID)
+      << "Expected void return type";
+  ASSERT_EQ(return_node->parameters.size(), 0) << "Expected no parameters";
+}
+
+TEST(FunctionDeclaration, IntReturnNoArgs) {
+  Tokens tokens;
+  tokens.emplace_back(std::make_unique<DataTypeToken>(Datatype::INTEGER));
+  tokens.emplace_back(std::make_unique<IdentifierToken>("min_heltalsfunktion"));
+  tokens.emplace_back(std::make_unique<OpenParenToken>());
+  tokens.emplace_back(std::make_unique<ClosedParenToken>());
+  tokens.emplace_back(std::make_unique<TerminatorToken>());
+
+  auto ts = TokenStream{std::move(tokens)};
+  auto ast = parse_tokens(ts);
+
+  ASSERT_TRUE(ast.statements.size() == 1) << fmt::format(
+      "Expected program with 1 statement, actual: {}", ast.statements.size());
+  auto return_node =
+      dynamic_cast<FunctionDeclarationNode *>(ast.statements[0].get());
+  ASSERT_NE(return_node, nullptr) << "Expected a FunctionDeclarationNode";
+  ASSERT_EQ(return_node->returnType, Datatype::INTEGER)
+      << "Expected integer return type";
+  ASSERT_EQ(return_node->parameters.size(), 0) << "Expected no parameters";
 }
