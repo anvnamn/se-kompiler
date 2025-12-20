0c56fc5fa756a2504bfaebf9286d443794fb3437
anvnamn <85112292+anvnamn@users.noreply.github.com>
2025-12-20 22:30:11 +0100

    Add tests for codegen stack size

--- tests/codegen_test.cpp
+++ tests/codegen_test.cpp
@@ -4,6 +4,7 @@
 #include <fmt/format.h>
 #include <gtest/gtest.h>
 #include <memory>
+#include <regex>
 
 TEST(Codegen, ReturnIntegerLiteral) {
   ProgramBuilder builder;
@@ -199,3 +200,42 @@ TEST(Codegen, AssignIntegerVar) {
   const auto return_status = run_assembly(actual_as);
   ASSERT_EQ(return_status, assigned_value);
 }
+
+class StackSizeParams : public ::testing::TestWithParam<std::tuple<int, int>> {
+};
+
+TEST_P(StackSizeParams, StackSize) {
+
+  int size_annotation = std::get<0>(GetParam());
+  int expected_code_size = std::get<1>(GetParam());
+
+  fmt::print("Testing stack size annotation: {}, expecting codegen size: {}\n",
+             size_annotation, expected_code_size);
+
+  ProgramBuilder builder;
+  builder.set_main_scope_stack_size(size_annotation);
+  auto program = builder.build();
+
+  auto codegen = Codegen();
+  const auto actual_as = codegen.generate_assembly(*program);
+  fmt::print("Generated assembly:\n{}", actual_as);
+  // use regex to find stack size allocation
+  std::regex stack_size_regex(R"(\s*sub\s+\$(\d+),\s+\%rsp)");
+  std::smatch match;
+  if (std::regex_search(actual_as, match, stack_size_regex)) {
+    const auto actual_code_size = std::stoi(match[1].str());
+    ASSERT_EQ(actual_code_size, expected_code_size);
+  } else {
+    if (expected_code_size == 0) {
+      // No stack allocation expected, test passes
+      return;
+    }
+    FAIL() << "No stack size allocation found in assembly";
+  }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    Codegen, StackSizeParams,
+    ::testing::Values(std::make_tuple(0, 0), std::make_tuple(4, 16),
+                      std::make_tuple(8, 16), std::make_tuple(13, 16),
+                      std::make_tuple(16, 16), std::make_tuple(24, 32)));
